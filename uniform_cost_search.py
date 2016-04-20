69a080dc06bc91403caa5a04b68ca327a95c59ae
Richard Hsu <rhsu@gwu.edu>
2016-04-20 16:21:50 -0400

    Fixed unifrom cost search algorithm

--- uniform_cost_search.py
+++ uniform_cost_search.py
@@ -5,11 +5,15 @@
 
 graph = {
 	
-	'A': [['B', 250], ['C', 270]],
-	'B': [['B', 250], ['C', 300]],
-	'C': [['A', 270], ['D', 300]],
-	'D': [['C', 420]]
+	'A': [['B', 10], ['C', 270]],
+	'B': [['A', 10], ['C', 20]],
+	'C': [['A', 270], ['B', 20], ['D', 420]],
+	'D': [['C', 420], ['E', 125]],
+	'E': [['D', 125]]
 }
+
+
+
 
 def uniform_cost_search(graph, start, goal):
 	node = start
@@ -17,53 +21,85 @@ def uniform_cost_search(graph, start, goal):
 	# initialize the cost
 	cost = 0
 
+	frontier = []
+
+	# create a list for all the frontier node
+	frontier_node_costs = []
+
+	count = 0
+
+	stored_cost = 0
+
 	# initialize a queue containing node only
-	frontier = [node]
-	print(frontier)
-	print(len(frontier))
+	frontier.append(node)
+	#print(frontier)
+	#print(len(frontier))
 
 	# initialize a queeu containing the visited nodes
 	visited = []
 
 	while True: 
-		print(len(frontier))
+		
+		# reset count
+		count = 0
+		#print(len(frontier))
 		if len(frontier) == 0:
 			print("Sorry, no path exists!")
 			return 
+
 		# choose the lowest cost node from the frontier
-		node = frontier.pop()
+		if len(frontier_node_costs) != 0:
+
+			first_cost = frontier_node_costs[0]
+			index = 0
+			loop_counter = 0
+			for cost in frontier_node_costs:
+				if (cost < first_cost):
+					index = loop_counter
+				loop_counter += 1
+		
+			node = frontier.pop(index)
+		else:
+			node = frontier.pop()
 
 		if node == goal:
 			print("You reached the goal!")
 			return visited
 		# add that node to the visited nodes queue
 		visited.append(node)
+		print("--- Visited ---")
+		print(visited)
+		print("--- ---")
+		
 
-		count = 0
+		#for each of the node's neighbors n 
+		for neighbor in graph[node]:
+			print("--- Neighbor ---")
+			print(neighbor)
+			print("--- ---")
 
-		# for each of the node's neighbors n 
-		# for neighbor in graph[node]:
-		# 	print(neighbor)
+			#if neighbor is goal, then we have reached it
+			if neighbor[0] == goal:
+				print("You reached the goal!")
+				visited.append(neighbor[0])
+				return visited
 
-		# 	# if neighbor is goal, then we have reached it
-		# 	# if neighbor[0] == goal:
-		# 	# 	print("You reached the goal!")
-		# 	# 	return frontier
+			# check that it is not in the visited
+			if neighbor[0] not in visited:
+				# if this is the first iteration, add the node to the frontier
+				if neighbor[0] not in frontier:
+					frontier.append(neighbor[0])
+					frontier_node_costs.append(neighbor[1])
+					cost = neighbor[1]
+				elif cost > neighbor[1]:
+					frontier.pop()
+					frontier.append(neighbor[0])
+					frontier_node_costs.append(neighbor[1])
+			print("--- Frontier ---")
+			print(frontier)
+			print("--- ---") 
+			# increment counter
+			#count = count + 1
 
-		# 	# check that it is not in the visited
-		# 	if neighbor[0] not in visited:
-		# 		# check that it is not in the frontier
-		# 		if neighbor[0] not in frontier:
-		# 			frontier.append(neighbor)
-		# 			stored_cost = neighbor[1]
-		# 			#print(cost)
-		# 			#print(frontier)
-		# 		elif neighbor[1] > stored_cost:
-		# 			# remove the node in frontier
-		# 			frontier.pop()
-		# 			# add the new node with the lower cost
-		# 			frontier.append(neighbor)
-		#return frontier
-
-print(uniform_cost_search(graph, 'A', 'D'))
+print(uniform_cost_search(graph, 'A', 'E'))
 	
