bb6bec67b0169af16b22eb7f7e26bec093bc6b47
Richard Hsu <rhsu@gwu.edu>
2016-04-28 11:19:50 -0400

    Modified A* to include straightline distance

--- A*-search.py
+++ A*-search.py
@@ -3,23 +3,69 @@
 
 # sample graph 
 
-graph = {
+# graph = {
 	
-	'A': [['B', 10], ['C', 50]],
-	'B': [['A', 10], ['C', 20]],
-	'C': [['A', 50], ['B', 20], ['D', 420]],
-	'D': [['C', 420], ['E', 125]],
-	'E': [['D', 125]]
+# 	'A': [['B', 10], ['C', 50]],
+# 	'B': [['A', 10], ['C', 20]],
+# 	'C': [['A', 50], ['B', 20], ['D', 420]],
+# 	'D': [['C', 420], ['E', 125]],
+# 	'E': [['D', 125]]
+# }
+
+graphWithDirect = {
+	
+	'JFK': [['SFO', 2566], ['LAX', 2458], ['MSP', 1009], ['ORD', 720], ['CLT', 545], 
+	['ATL', 762], ['DFW', 1380], ['SLC', 1970], ['PHX', 2139], ['DTW', 485], ['SEA', 679]],
+	'SFO': [['JFK', 2566], ['LAX', 339], ['MSP', 1586], ['ORD', 1847], ['PHX', 652], 
+	['ATL', 2135], ['DFW', 1468], ['SLC', 599], ['DTW', 2083], ['SEA', 679]],
+	'LAX': [['JFK', 2458], ['SFO', 339]],
+	'MSP': [['JFK', 1009], ['SFO', 1586]],
+	'ORD': [['JFK', 720], ['SFO', 1847], ['PHX', 1440]],
+	'CLT': [['JFK', 545], ['SFO', 2292]],
+	'ATL': [['JFK', 762], ['SFO', 2135]],
+	'DFW': [['JFK', 762], ['SFO', 1468]],
+	'SLC': [['JFK', 1970], ['SFO', 599]],
+	'PHX': [['JFK', 2139], ['SFO', 652], ['ORD', 1440]],
+	'DTW': [['JFK', 485], ['SFO', 2083]],
+	'SEA': [['SEA', 2397], ['SFO', 679]]
+
+
+
+}
+
+graphWithoutDirect = {
+
+	'JFK': [['LAX', 2458], ['MSP', 1009], ['ORD', 720], ['CLT', 545], 
+	['ATL', 762], ['DFW', 1380], ['SLC', 1970], ['PHX', 2139], ['DTW', 485], ['SEA', 679]],
+	'SFO': [['LAX', 339], ['MSP', 1586], ['ORD', 1847], ['PHX', 652], 
+	['ATL', 2135], ['DFW', 1468], ['SLC', 599], ['DTW', 2083], ['SEA', 679]],
+	'LAX': [['JFK', 2458], ['SFO', 339]],
+	'MSP': [['JFK', 1009], ['SFO', 1586]],
+	'ORD': [['JFK', 720], ['SFO', 1847], ['PHX', 1440]],
+	'CLT': [['JFK', 545], ['SFO', 2292]],
+	'ATL': [['JFK', 762], ['SFO', 2135]],
+	'DFW': [['JFK', 762], ['SFO', 1468]],
+	'SLC': [['JFK', 1970], ['SFO', 599]],
+	'PHX': [['JFK', 2139], ['SFO', 652], ['ORD', 1440]],
+	'DTW': [['JFK', 485], ['SFO', 2083]],
+	'SEA': [['SEA', 2397], ['SFO', 679]]
+
 }
 
 
-straight_line_distance_from_A_to_D = {
+straight_line_distance_from_airport_to_SFO = {
 	
-	'A': 460,
-	'B': 420,
-	'C': 420,
-	'D': 0,
-	'E': 125
+	'JFK': 2581.21,
+	'LAX': 337.9,
+	'MSP': 1586.82,
+	'ORD': 1843.15,
+	'CLT': 2292.27,
+	'ATL': 2134.8,
+	'DFW': 1462.51,
+	'SLC': 598.54,
+	'PHX': 651.16,
+	'DTW': 2075.12,
+	'SEA': 679.36
 }
 
 
@@ -59,9 +105,11 @@ def A_star_search(graph, start, goal):
 		# choose the lowest cost node from the frontier
 		if len(frontier_node_costs) != 0:
 
-			first_cost = frontier_node_costs[0] + straight_line_distance_from_A_to_D[frontier[0]]
+			first_cost = frontier_node_costs[0] + straight_line_distance_from_airport_to_SFO[frontier[0]]
+			print("--- First cost ---")
 			print(first_cost)
-			index = 0
+			print("---  ---")
+			index = 1
 			loop_counter = 0
 
 			heuristic_cost = 0
@@ -69,9 +117,17 @@ def A_star_search(graph, start, goal):
 			# need to calculate f(n) from cost and estimated cost to goal
 
 			for cost in frontier_node_costs[1:len(frontier_node_costs)]:
-				heuristic_cost = cost + straight_line_distance_from_A_to_D[frontier[index]]
+				heuristic_cost = cost + straight_line_distance_from_airport_to_SFO[frontier[index]]
 				#print(straight_line_distance_from_A_to_D[frontier[index]])
+				print("--- Cost ---")
+				print(cost)
+				print("---  ---")
+				print("--- Straight Line Distance ---")
+				print(straight_line_distance_from_airport_to_SFO[frontier[index]])
+				print("---  ---")
+				print("--- Heuristic cost ---")
 				print(heuristic_cost)
+				print("---  ---")
 				if (heuristic_cost < first_cost):
 					index = loop_counter
 				loop_counter += 1
@@ -121,5 +177,5 @@ def A_star_search(graph, start, goal):
 			# increment counter
 			#count = count + 1
 
-print(A_star_search(graph, 'A', 'D'))
+print(A_star_search(graphWithoutDirect, 'JFK', 'SFO'))
 	
